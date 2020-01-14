2ac4811cf6000da6b128959c8eb867b8671a0693
Kyle-1002 <zhlai@ucsd.edu>
2020-01-13 19:30:54 -0800

    try

---
+++ hello/hello/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by LemuelLai on 1/13/20.
+//  Copyright © 2020 LemuelLai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    //that's fine.
+    std::cout << "Hello, World!\n";
+    return 0;
+}
